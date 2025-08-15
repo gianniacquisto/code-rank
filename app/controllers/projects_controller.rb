7af4abecac5b34a8e0389760fd9b07f42f30a34b
Gianni <gianni.acquisto@gmail.com>
2025-08-15 19:21:55 +0200

    sortable table - server side

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,17 @@
 class ProjectsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @projects = Project.all
+    @projects = Project.order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    Project.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
