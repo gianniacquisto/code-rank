69286b42b057f16973a63ce9791551d27e1693b3
Gianni <gianni.acquisto@gmail.com>
2025-08-17 18:34:39 +0200

    tests and rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [ :new, :create ]
   resource :session
   resources :passwords, param: :token
   resources :projects
