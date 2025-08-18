469ffb271d2001d745b3b28d8bb47f516dd7dc44
Gianni <gianni.acquisto@gmail.com>
2025-08-18 21:20:46 +0200

    feat: Remove project sorting functionality
    This commit removes the ability for users to sort projects on the index page.
    
    The sorting dropdowns have been removed from the view, and the corresponding backend logic, including the `sort_column` and `sort_direction` helper methods, has been deleted from the `ProjectsController`. Projects are now fetched in their default order, simplifying the code and user interface.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,8 @@
 class ProjectsController < ApplicationController
   allow_unauthenticated_access only: %i[ index ]
-  helper_method :sort_column, :sort_direction
 
   def index
-    @projects = Project.order(sort_column + " " + sort_direction)
+    @projects = Project.all
   end
 
   def new
@@ -21,14 +20,6 @@ class ProjectsController < ApplicationController
   end
 
   private
-
-  def sort_column
-    Project.column_names.include?(params[:sort]) ? params[:sort] : "name"
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
 
   def project_params
       params.expect(project: [ :name, :category, :url ])
