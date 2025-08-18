7dcb68a3fbb836f9c8fc9e5e2e961777347f360e
Gianni <gianni.acquisto@gmail.com>
2025-08-18 20:52:41 +0200

    renamed migrations to follow conventions

--- db/migrate/20250818113403_add_fields_to_votes.rb
+++ db/migrate/20250818113403_add_fields_to_votes.rb
@@ -1,4 +1,4 @@
-class AddFieldsToVote < ActiveRecord::Migration[8.0]
+class AddFieldsToVotes < ActiveRecord::Migration[8.0]
   def change
     add_column :votes, :vote, :integer
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_18_122421) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_18_184946) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
-    t.integer "stars"
   end
 
   create_table "sessions", force: :cascade do |t|
