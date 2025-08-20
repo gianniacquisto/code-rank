470b0ba074cc3553d8e93eec23f6117e044d2108
Gianni <gianni.acquisto@gmail.com>
2025-08-20 22:54:23 +0200

    removed some columns from the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_18_184946) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_20_205316) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "category"
+  end
+
+  create_table "repositories", force: :cascade do |t|
+    t.text "name", null: false
+    t.index ["name"], name: "idx_repositories_name"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -26,13 +29,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_184946) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
-  create_table "softwares", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
