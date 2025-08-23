4719c80790458ae8e426a7e8a7c3a6956455c068
Gianni <gianni.acquisto@gmail.com>
2025-08-23 20:12:41 +0200

    made some UX enhancements

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,7 @@ class VotesController < ApplicationController
     @project = Project.find(params[:project_id])
     @vote = @project.votes.find_or_initialize_by(user: Current.session.user)
     @vote.vote = params[:vote]
-
-    if @vote.save
-      redirect_to projects_path, notice: "Vote was successfully recorded."
-    else
-      redirect_to projects_path, alert: "There was an error recording your vote."
-    end
+    @vote.save
   end
 
   private
