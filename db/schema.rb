44568cdde008c0d04a7878f07659e83eefb131c8
Gianni <gianni.acquisto@gmail.com>
2025-12-21 22:21:39 +0100

    created technology model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_20_205316) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_21_201222) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -26,11 +26,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_20_205316) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "adminFlag"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
