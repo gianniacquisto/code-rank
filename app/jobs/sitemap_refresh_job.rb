37f9f05e11bd6920f0ff3acf19166045f55e0f0a
Gianni <gianni.acquisto@gmail.com>
2026-06-24 22:36:47 +0200

    chore: remove post-deploy hook (redundant with daily job)

--- app/jobs/sitemap_refresh_job.rb
+++ app/jobs/sitemap_refresh_job.rb
@@ -4,8 +4,13 @@ class SitemapRefreshJob < ApplicationJob
   queue_as :default
 
   def perform
+    Rails.logger.info "Regenerating sitemap..."
     require "sitemap_generator"
     SitemapGenerator::Interpreter.run(config_file: Rails.root + "config/sitemap.rb")
     SitemapGenerator::Sitemap.finalize!
+    Rails.logger.info "Sitemap regenerated successfully"
+  rescue => e
+    Rails.logger.error "Sitemap regeneration failed: #{e.message}"
+    raise
   end
 end
